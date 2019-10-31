e6b250e3160dd593828b4420e5d03ac7cef27b10
Cesar M <cmagallon@ucol.mx>
2019-10-31 04:32:40 -0600

    [add]test issue #1

---
+++ cypress/integration/issue1.spec.js
@@ -0,0 +1,14 @@
+describe('Agrega al listado principal de personas el campo “Dirección” y quitar el campo “Fecha”',function() {
+ it('Visitar pagina de paiza', function() {
+    cy.visit('https://directorio-en-php.paiza-user.cloud/~ubuntu/directorio-en-php/index.php')
+ })
+ it('Realizar insercion de usuario', function() {
+    cy.get('thead > tr > :nth-child(5) > .btn').click()
+    cy.get('.col-md-7 > .form-control')
+        .type('Cesar')
+    cy.get('.col-md-11 > .form-control')
+        .type('real del valle')
+    cy.get('.modal-footer > .btn-primary').click()
+ })
+
+})
